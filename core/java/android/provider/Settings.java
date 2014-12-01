abd8e29e0468c53b3f72d7ffb16cc12e2a06e81f
Jon Evans <jon@craftyjon.com>
2014-11-30 20:55:42 -0500

    Wake on plug [1/2]
    Change-Id: I5e50a7fd351857d76c1c2fd13c6e737d8902a2f3
    Signed-off-by: devabhixda <devabhixda@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4119,6 +4119,13 @@ public final class Settings {
          */
         public static final String SHOW_VOLTE_ICON = "volte_icon";
 
+       /**
+         * Whether to wake the display when plugging or unplugging the charger
+         *
+         * @hide
+         */
+        public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
